c6f148b2741f4530a95e7eb819b67e4be3e26ef8
MapreeDev <66095855+MapreeDev@users.noreply.github.com>
2023-07-29 10:06:51 -0300

    ...

--- src/components/BtnVoltar/index.jsx
+++ src/components/BtnVoltar/index.jsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { FaAngleUp } from "react-icons/fa";
 import styles from './index.module.css'
+
 export default function BtnVoltar(){
+    const handleReturnToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" })
+    }
     return(
-        <div className={styles.content_voltar}>
+        <div className={styles.content_voltar} onClick={handleReturnToTop}>
             <p>Voltar para o topo</p>
             <div>
                 <FaAngleUp/>
